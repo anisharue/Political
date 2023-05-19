bbd25dfba3e626099fcd654cd72a784b0844d4d7
Anisha <anisharuvimbo@gmail.com>
2023-05-19 11:28:49 +0200

    Added docstrings to registration models.py

--- registration/models.py
+++ registration/models.py
@@ -3,4 +3,11 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
+    """
+    A model representing user profile information.
+
+    Attributes:
+        user (User): A one-to-one relationship to the built-in Django User model.
+    """
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
